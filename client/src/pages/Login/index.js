825ca8884227b79871115b59eb7000f5294072c2
Vinicius Anderle <vinianderle00@gmail.com>
2023-11-19 22:07:43 -0300

    Cookies, sessions middleware
    Lado do Servidor (Node.js / Express):
    Configuração do Firebase:
    
    O código configura o Firebase Admin SDK no servidor usando as credenciais fornecidas em serviceAccountKey.json.
    Express e Middleware:
    
    O servidor utiliza o framework Express para lidar com rotas e requisições HTTP.
    cookie-parser é usado para analisar cookies recebidos nas requisições.
    express-session é configurado para gerenciar sessões de usuário.
    Middleware de Verificação de Autenticação (checkAuth):
    
    Um middleware checkAuth é definido para verificar se um usuário está autenticado em cada requisição. Ele utiliza a sessão para armazenar informações do usuário.
    Rota de Login (/api/login):
    
    Quando um usuário faz login com sucesso, um token de sessão é gerado usando o Firebase Authentication.
    Esse token é então armazenado em um cookie e as informações do usuário (como UID) são armazenadas na sessão.
    Rota de Logout (/api/logout):
    
    Ao chamar esta rota, a sessão do usuário é destruída e o cookie é removido, efetuando o logout.
    Rota Protegida (/api/protected):
    
    Uma rota protegida é definida, exigindo autenticação. O middleware checkAuth é utilizado para verificar se o usuário está autenticado antes de conceder acesso.
    Lado do Cliente (React):
    Componente de Login:
    
    Um componente React chamado Login é definido para lidar com o formulário de login.
    Ele utiliza o hook useSignInWithEmailAndPassword do react-firebase-hooks/auth para autenticar o usuário.
    Verificação de Login no Carregamento da Página (useEffect):
    
    Um efeito colateral é usado para verificar se o usuário está autenticado ao carregar a página, com base no token de sessão armazenado no localStorage.
    Funções de Login e Logout:
    
    handleSignIn: É chamada ao enviar o formulário de login. Utiliza o Firebase Authentication para autenticar o usuário e redireciona após o login bem-sucedido.
    handleSignOut: É chamada ao clicar no botão de logout. Remove o cookie e limpa o localStorage.
    Renderização Condicional de Componentes:
    
    O botão no formulário muda dinamicamente com base no estado de autenticação. Se o usuário estiver autenticado, é exibido um botão para desconectar; caso contrário, é exibido um botão para fazer login.

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth"; // Importe useSignInWithEmailAndPassword
-import { signOut } from "firebase/auth"; // Importe signOut
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { signOut } from "firebase/auth";
 import arrowImg from "../../assets/arrow.svg";
 import { auth } from "../../services/firebaseConfig";
 
@@ -11,8 +11,15 @@ export function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
-  const [signInWithEmailAndPassword, user, loading, error] =
-    useSignInWithEmailAndPassword(auth);
+  const [signInWithEmailAndPassword, user, loading, error] = useSignInWithEmailAndPassword(auth);
+
+  // Verifica o login ao carregar a página
+  useEffect(() => {
+    const sessionCookie = localStorage.getItem('session');
+    if (sessionCookie) {
+      // Faça a verificação do cookie e defina o estado do usuário se estiver autenticado
+    }
+  }, []);
 
   function handleSignIn(e) {
     e.preventDefault();
@@ -23,6 +30,8 @@ export function Login() {
   }
 
   function handleSignOut() {
+    // Remova o cookie e limpe o localStorage
+    localStorage.removeItem('session');
     signOut(auth)
       .then(() => {
         // O usuário foi desconectado com sucesso.
@@ -34,54 +43,51 @@ export function Login() {
       });
   }
 
-  return (<>
-    <div className="cont">
-      
-      <h2>Por favor, digite suas informações de login</h2>
-
-      <form>
-        <div className="inputcont">
-          <label htmlFor="email">E-mail</label>
-          <input
-            type="text"
-            name="email"
-            id="email"
-            placeholder="E-mail"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-
-        <div className="inputcont">
-          <label htmlFor="password">Senha</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="********************"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-
-        {user ? (
-          <button className="button" onClick={handleSignOut}>
-            Desconectar
-          </button>
-        ) : (
-          <>
-            <button className="button" onClick={handleSignIn}>
-              Entrar <img src={arrowImg} alt="->" />
+  return (
+    <>
+      <div className="cont">
+        <h2>Por favor, digite suas informações de login</h2>
+        <form>
+          <div className="inputcont">
+            <label htmlFor="email">E-mail</label>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              placeholder="E-mail"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="inputcont">
+            <label htmlFor="password">Senha</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="********************"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          {user ? (
+            <button className="button" onClick={handleSignOut}>
+              Desconectar
             </button>
-            <div className="footer">
-              <p>Você não tem uma conta?</p>
-              <Link to="/register">Crie a sua conta aqui</Link>
-             
-            </div>
-            <Link  className="voltar" to="/">Voltar para o inicio      
-        </Link>
-          </>
-        )}
-      </form>
-    </div>
-  </>
+          ) : (
+            <>
+              <button className="button" onClick={handleSignIn}>
+                Entrar <img src={arrowImg} alt="->" />
+              </button>
+              <div className="footer">
+                <p>Você não tem uma conta?</p>
+                <Link to="/register">Crie a sua conta aqui</Link>
+              </div>
+              <Link className="voltar" to="/">
+                Voltar para o início
+              </Link>
+            </>
+          )}
+        </form>
+      </div>
+    </>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,22 +1,81 @@
-const path = require("path");
-const express = require("express");
-
-const PORT = process.env.PORT || 3001;
+const express = require('express');
+const admin = require('firebase-admin');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const serviceAccount = require('./path/to/your/serviceAccountKey.json');
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use(express.static(path.resolve(__dirname, "../client/build")));
-
-// Lidar com as solicitações GET feitas à rota /api
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+// Inicialize o Firebase Admin SDK
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://console.firebase.google.com/u/0/project/estacaodigital-7533a/authentication/users?hl=pt',
 });
 
-// Todas as outras solicitações GET não tratadas retornarão nosso app em React
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+// Configuração do express-session
+app.use(cookieParser());
+app.use(
+  session({
+    secret: 'secretpassword', // Altere isso para uma string mais segura em um ambiente de produção
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false, maxAge: 60 * 60 * 1000 }, // Configuração do cookie (ajuste conforme necessário)
+  })
+);
+
+// Middleware para verificar a autenticação do usuário em cada requisição
+const checkAuth = (req, res, next) => {
+  if (req.session && req.session.userData) {
+    req.user = req.session.userData;
+    return next();
+  } else {
+    console.error('Erro ao verificar sessão');
+    res.status(401).json({ error: 'Não autorizado' });
+  }
+};
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Rota para login
+app.post('/api/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const userRecord = await admin.auth().getUserByEmail(email);
+    const uid = userRecord.uid;
+
+    // Crie um token personalizado que expire em 1 hora
+    const sessionCookie = await admin.auth().createSessionCookie(uid, { expiresIn: 60 * 60 * 1000 });
+
+    // Configure o cookie na resposta e armazene as informações do usuário na sessão
+    req.session.userData = {
+      uid: uid,
+      // Outras informações do usuário que você deseja armazenar na sessão
+    };
+
+    res.cookie('session', sessionCookie, { maxAge: 60 * 60 * 1000, httpOnly: true });
+    res.json({ status: 'success' });
+  } catch (error) {
+    console.error('Erro ao fazer login:', error);
+    res.status(401).json({ error: 'Credenciais inválidas' });
+  }
+});
+
+// Rota para logout
+app.post('/api/logout', (req, res) => {
+  // Limpe as informações da sessão e remova o cookie
+  req.session.destroy();
+  res.clearCookie('session');
+  res.json({ status: 'success' });
+});
+
+// Rota protegida - somente usuários autenticados podem acessar
+app.get('/api/protected', checkAuth, (req, res) => {
+  res.json({ message: 'Rota protegida, usuário autenticado', user: req.user });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
